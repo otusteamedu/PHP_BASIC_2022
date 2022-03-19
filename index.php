abf0f044ccbfbd6b730dc17b825c3fac9b6007cc
Denis A Chelnik <denis@chelnik.ru>
2022-03-19 20:48:51 +0300

    Работа над ДЗ
    Выполнил задание 1.

--- index.php
+++ index.php
@@ -5,6 +5,24 @@
  * Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
  */
 
+function addition(float $value1, float $value2) {
+    return ($value1 + $value2);
+}
+
+function subtraction(float $value1, float $value2) {
+    return ($value1 - $value2);
+}
+
+function multiplication(float $value1, float $value2) {
+    return ($value1 * $value2);
+}
+
+function division(float $value1, float $value2) {
+    if ($value2 == 0) 
+        return "На ноль делить нельзя!";
+    return ($value1 / $value2);
+}
+
 
 /**
  * ЗАДАНИЕ 2
@@ -26,6 +44,21 @@ echo "© ".$nowYear." OTUS PHP Developer. BASIC";
  * ЗАДАНИЕ 4
  * Написать функцию, которая вычисляет текущее время и возвращает его в формате с правильными склонениями, например: 22 часа 15 минут 21 час 43 минуты
  */
+echo "<h2>Задание 4:</h2>";
 
+function declination($nowHours, $nowMinutes) {
+    if ($nowHours==1 || $nowHours==21) 
+        $declinationHours = " час";
+    elseif (($nowHours>=2 && $nowHours<=4) || ($nowHours>=22 && $nowHours<=24)) 
+        $declinationHours = " часа";
+    else
+        $declinationHours = " часов";
+
+.......
+
+    echo $nowHours . $declinationHours . " " . $nowMinutes . $declinationMinutes;
+}
+
+declination(date("H"), date("i"));
 
 ?>
