e17705a4bd7111ad40fa64613ed2333fb240e86a
Igor Tsyganok <ciganokigor@gmail.com>
2022-04-02 23:20:03 +0500

    скрипт загрузки картинок

---
+++ upload.php
@@ -0,0 +1,18 @@
+<?php
+/* $uploaddir = 'C:\\OpenServer\\userdata\\temp\\upload\\'; */
+$uploaddir = 'c:\\OpenServer\\domains\\otus.php\\image\\'; 
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Файл корректен и был успешно загружен.\n";
+} else {
+    echo "Возможная атака с помощью файловой загрузки!\n";
+}
+
+echo 'Некоторая отладочная информация:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
