713b49114cde2206b8a3b9898b568db13600908a
Denis A Chelnik <denis@chelnik.ru>
2022-04-06 16:48:35 +0300

    Работа над ДЗ
    Подключил Bootstrap. Прописал составные части сетки Bootrstrap. Добавил иконки. Добавил элементы в структуру. Сделал шаблонную структуру.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$op = $_GET['op'];
+
+// include("./config/db.inc.php");
+
+// Подгружаем требуемый html-шаблон, возвращаем содержимое
+function getTemplate($template) {
+    ob_start();
+    include ("./templates/".$template.".html."); // Отправляем в буфер содержимое файла
+    $variables = ob_get_clean(); // Очищаем буфер и возвращаем содержимое
+    return $variables; // Возвращение текста из файла
+}
+
+function main()
+{
+    global $fyb, $db;
+    // подгружаем шаблон шапки сайта
+    include_once("./templates/main.head.html");
+
+    // подгружаем шаблон навигационной панели
+    include_once("./templates/main.navbar.html");
+
+    // подгружаем шаблон боковой панели
+    include_once("./templates/main.container.sidebar.html");
+
+    // подгружаем шаблон основного содержимого
+    include_once("./templates/main.container.menu.html");
+
+    // подгржаем шаблон подвала сайта
+    include("./templates/main.footer.html");
+}
+
+switch($op) {
+    default:
+        main();
+        break;
+}
