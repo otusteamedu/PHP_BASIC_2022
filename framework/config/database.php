100645d8c6fd264d4a5a694a9606c607d1e7936b
Igor Tsyganok <ciganokigor@gmail.com>
2022-06-08 15:46:01 +0500

    Update database.php

--- framework/config/database.php
+++ framework/config/database.php
@@ -1,11 +1,15 @@
 <?php
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->Load();
+        
     return [
-        'driver' => '',
-        'host' => '',
-        'port' => ,
-        'db' => '',
+        'driver' => $_ENV['DRIVER'],
+        'host' => $_ENV['HOST'],
+        'port' => $_ENV['PORT'],
+        'db' => $_ENV['DB'],
         # for doctrine
-        'dbname' => '',
-        'user' => '',
-        'password' => ''
+        'dbname' => $_ENV['DB'],
+        'user' => $_ENV['USERDB'],
+        'password' => $_ENV['PASSWORD']
     ];
