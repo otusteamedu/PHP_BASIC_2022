24dc05c05aa0ccc727a3ee233b3107aa683de235
Mihail Petrakov <mihailpetrakov065@gmail.com>
2022-07-06 13:36:12 +0300

    homework16

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+require_once ("image_resizer.php");
+
+$gallery = 'gallery';
+$galleryMin = 'galleryMin';
+
+
+function iWantToBeLogged(string $picture)
+{
+    $logFile = 'txt/LogLoadImages.txt';
+    $result = 'Добавили в галлерею фото ' . $picture . ', дата и время загрузки: ' . date('Y-m-d H:i:s') . PHP_EOL;
+    file_put_contents($logFile, $result, FILE_APPEND);
+}
+
+function addedImages(array $pictureFile, string $gallery)
+{
+    if (isset($pictureFile['picture']) && $pictureFile['picture']['size'] > 0) {
+        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        $ext = pathinfo($pictureFile['picture']['name'], PATHINFO_EXTENSION);
+
+        if (in_array($ext, $allowedExtensions)) {
+            $uniq_name = uniqid() . $pictureFile['picture']['name'];
+
+            if ((move_uploaded_file($pictureFile['picture']['tmp_name'], $gallery . $uniq_name))) {
+                SetImgSize('gallery/'.$uniq_name, 350, 240, 1, 'galleryMin/'.$uniq_name);
+                unset($uniq_name);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    return false;
+
+}
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+include_once "functions.php";
+
+$gallery = 'gallery/';
+$galleryMin = 'galleryMin/';
+
+if (isset($_FILES['picture']) && $_FILES['picture']['size'] > 0) {
+    addedImages($_FILES, $gallery);
+}
+
+$pictures = array_diff(scandir($gallery), ['.', '..']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body><div class="services">
+    <div class="container">
+        <div class="block-main-caption">
+            <h1 class="main-caption">Фотогаллерея</h1>
+        </div>
+
+        <div class="flex-container">
+            <?php
+            if (!empty($pictures)) {
+                foreach ($pictures as $picture) {
+                    ?>
+                    <div class="item">
+                        <a href="<?php echo($gallery . $picture); ?>" target="_blank">
+                            <img class="image-gallery" style="width:350px;" src="<?php echo($galleryMin . $picture); ?>"
+                                 alt="<?php echo($picture); ?>" />
+                        </a>
+                    </div>
+                    <?php
+                }
+            } else {
+                ?>
+                <h3>Галерея пока пуста</h3>
+                <?php
+            }
+            ?>
+            <div class="block-button">
+                <form action="index.php" method="post" enctype="multipart/form-data">
+                    <input type="file" id="idPicture" name="picture"/>
+                    <input type="submit" value="Отправить!">
+                </form>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
