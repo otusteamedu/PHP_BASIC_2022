a649b2820a901480339af17cddb05b07d7bf0149
Vladimir Rumiancev <cto@grape24.ru>
2022-07-20 21:05:49 +0300

    комменты в функции

--- function/function.php
+++ function/function.php
@@ -1,10 +1,18 @@
 <?php
 function writeLog (string $text):void {
     $text = date('Y-m-d H:i:s')."_". $text . PHP_EOL;
-    $file = fopen('C:\repositories\PHP_BASIC_2022\log.txt','a');
+    $file = fopen('C:\repositories\PHP_BASIC_2022\log.txt','a'); //указать прямой путь на сервере
     fwrite($file,$text);
     fclose($file);
 };
-
+function putDataCsv (array $userData, mixed $img): void {
+    $file = fopen('C:\repositories\PHP_BASIC_2022\data.csv','a'); //указать прямой путь на сервере
+    fputcsv(
+        $file,
+        $userData,
+        ";"
+    );
+    fclose($file);
+}
 
 ?>
