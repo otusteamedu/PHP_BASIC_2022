a2e458cd0e299721676352b24fa1df8f3960b72c
Mihail Petrakov <mihailpetrakov065@gmail.com>
2022-07-21 16:16:09 +0300

    Homework 17/2

---
+++ connect_db.php
@@ -0,0 +1,22 @@
+<?php
+require_once('config.php');
+function connect()
+{
+    return new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USERNAME, PASSWORD, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::FETCH_ASSOC => true
+    ]);
+}
+
+
+$db = connect();
+
+if (!empty($_POST) && array_key_exists('q', $_POST)) {
+    $result = $db->prepare('SELECT * FROM `library` WHERE `bookname` LIKE :query OR `author` LIKE :query');
+    $result->execute([':query' => '%' . $_POST['q'] . '%']);
+} else {
+    $result = $db->query('SELECT * FROM `library`');
+    $result->execute();
+}
+
+$list = $result->fetchAll(PDO::FETCH_ASSOC);
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('config.php');
+require_once('connect_db.php');
 ?>
 
 <form method="post" action="index.php">
