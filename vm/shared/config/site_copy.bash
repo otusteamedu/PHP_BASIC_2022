b4ed2c34c504bc4da94eab80dd85db15c9a56e7d
dimitryos <dimadb2@yandex.ru>
2022-10-04 17:33:36 +0300

    В provision-скрипт lобавлена команда создания директории для контента сайтов

--- vm/shared/config/site_copy.bash
+++ vm/shared/config/site_copy.bash
@@ -3,6 +3,4 @@
 # Перенос файлов проекта web-сайта из разделяемого раздела типа vboxfs в /var/www
 # Данный перенос требуется по причине ошибок в чтении web-сервером nginx файлов статического контента, если они находятся на виртуальном разделе типа vboxfs
 
-sudo mkdir /var/www
-sudo chown vagrant /var/www
-cp -r /shared/www/htdocs/* /var/www/
+cp -rn /shared/www/htdocs/* /var/www/
