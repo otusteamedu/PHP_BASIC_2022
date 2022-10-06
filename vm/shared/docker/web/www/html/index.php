54ffe20200ff13a4a19846ca24c6f2ccffbe2f8a
dimitryos <dimadb2@yandex.ru>
2022-10-06 16:24:23 +0300

    Переработана вёрстка

--- vm/shared/docker/web/www/html/index.php
+++ vm/shared/docker/web/www/html/index.php
@@ -17,6 +17,7 @@
     </header>
 
     <main>
+        <div id="tiles">
         <!-- плитка -->
         <div class="tile">
             <img class="feature_icon" src="/images/icon1.png" alt="значок Mobile Application">
@@ -77,19 +78,23 @@
             </p>
             <a href="#" class="expand" id="expand_6">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
         </div>
-    </main>
-
-    <footer>
-        <!-- блок навигации -->
+        </div>
+         <!-- блок навигации -->
         <div class="explore">
             <button type="button">Explore all <img src="images/arrow_r_beige.svg" alt="Стрелка вправо"></button>
         </div>
-        <hr>
+    </main>
+
+    <footer>
+        <nav>
+            <p class="nav_item"><a class="nav_link" href="type_conversion.php">Приведение типов</a></p>
+            <p class="nav_item"><a class="nav_link" href="var_exchange.php">Обмен значениями переменных</a></p>
+        </nav>
         <?php
             $dtiNow = new DateTimeImmutable('now');
             $strNow = $dtiNow->format('d.m, H:i');
         ?>
-        <p><em>Сформирована: <?=$strNow;?></em></p>
+        <p><em>Сформировано: <?=$strNow;?></em></p>
     </footer>
 </body>
 
