446baa68552b34eb1efbd853406034414a634c63
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2022-10-15 14:22:00 +0300

    Hw
    Hw

---
+++ 3 задание.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <section class="services">
+      <div class="container">
+        <h1>
+          Services that connect you to <br />
+          your users
+        </h1>
+        <div class="services-flex">
+          <div class="services-flex_item">
+            <img src="img/services-icon.svg" alt="icon" />
+            <h3>Mobile Application</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+          <div class="services-flex_item">
+            <img src="img/WA.svg" alt="icon" />
+            <h3>Web Application</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+          <div class="services-flex_item">
+            <img src="img/UI.svg" alt="icon" />
+            <h3>User Interface Design</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+          <div class="services-flex_item">
+            <img src="img/SaB.svg" alt="icon" />
+            <h3>Strategy and Branding</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+          <div class="services-flex_item">
+            <img src="img/PM.svg" alt="icon" />
+            <h3>Performance Marketing</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+          <div class="services-flex_item">
+            <img src="img/PrM.svg" alt="icon" />
+            <h3>Project Management</h3>
+            <p>
+              Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed
+              nam sem tellus erat.
+            </p>
+            <a href="#">Explplore →</a>
+          </div>
+        </div>
+      </div>
+      <div class="button-flex">
+        <button>Explore all →</button>
+      </div>
+    </section>
+      <footer style="color:aliceblue; text-align: center">
+        <?php echo date('Y-m-d H:i:s')?>
+      </footer>
+  </body>
+</html>
