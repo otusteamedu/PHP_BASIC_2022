5f3fca68fca8c7fcb806cf48657a98ee6adc3cb3
dimitryos <dimadb2@yandex.ru>
2022-10-16 23:19:04 +0300

    Разработана функция умножения

---
+++ shared/docker/web/www/html/src/multiply.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Функция умножения двух чисел
+ *
+ * @return int|float Произведение чисел.
+ */
+function multiply(int|float $a, int|float $b): int|float
+{
+    return $a * $b;
+}
