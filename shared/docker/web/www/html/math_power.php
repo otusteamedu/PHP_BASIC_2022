08b4947cc5bdf229639f84f77f03bc3f94dcb5bd
dimitryos <dimadb2@yandex.ru>
2022-10-16 23:23:01 +0300

    Разработан макет раздела иллюстрации работы функции возведения в степень

--- shared/docker/web/www/html/math_power.php
+++ shared/docker/web/www/html/math_power.php
@@ -2,22 +2,10 @@
     require('src/hours_rus.php');
     require('src/minutes_rus.php');
     require('src/power.php');
+
+    $title = 'ДЗ_14: Функции';
+    require('head.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-
-    <title>ДЗ_14: Функции</title>
-</head>
 
 <body>
     <header>
@@ -48,14 +36,7 @@
         <nav>
             <p class="nav_item"><a class="nav_link" href="/index.php">На главную</a></p>
         </nav>
-        <?php
-            $dtiNow = new DateTimeImmutable('now');
-            $dateNow = $dtiNow->format('d.m.Y');
-            $hoursNow = $dtiNow->format('H');
-            $minutesNow = $dtiNow->format('i');
-        ?>
-        <p><em> Сформировано: <?=$dateNow;?>, <?=hours_rus($hoursNow);?> <?=minutes_rus($minutesNow);?>. </em></p>
-        <p><em> PHP_VERSION: <?=$_SERVER['PHP_VERSION'];?> </em></p>
+       <?php include('foot.php'); ?>
     </footer>
 </body>
 
