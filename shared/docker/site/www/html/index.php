78b4b1d1d26a22a00aff1a3a29f103da23b5f1ba
dimitryos <dimadb2@yandex.ru>
2022-10-22 01:23:24 +0300

    Задание 6

--- shared/docker/site/www/html/index.php
+++ shared/docker/site/www/html/index.php
@@ -4,6 +4,7 @@
 
     $title = 'Connection services';
     require 'head.php';
+    require 'menu.php';
 ?>
 
 <body>
@@ -11,80 +12,96 @@
         <h1>Services that connect you to your users</h1>
     </header>
 
-    <main>
-        <div id="tiles">
-        <!-- плитка -->
-        <div class="tile">
-            <img class="feature_icon" src="/images/icon1.png" alt="значок Mobile Application">
-            <h3 class="tile_header">Mobile Application</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_1">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
+    <div class="d-flex">
+        <aside class="w-25">
+            <details>
+                <summary>Меню</summary>
+                <?php
+                    foreach ($menu as $razdel) {
+                        echo sprintf('<p class="menu_razdel"><a href="%s">%s</a></p>', $razdel[1], $razdel[0]);
+                        if ($razdel[2]) {
+                            echo '<ul>';
+                            foreach ($razdel[2] as $subrazdel) {
+                                echo sprintf('<li><a href="%s">%s</a></li>', $subrazdel[1], $subrazdel[0]);
+                            }
+                            echo '</ul>';
+                        }
+                    }
+                ?>
+            </details>
+        </aside>
 
-        <div class="tile">
-            <img class="feature_icon" src="images/icon2.png" alt="значок Web Application">
-            <h3 class="tile_header">Web Application</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_2">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
+        <main>
+            <div id="tiles">
+            <!-- плитка -->
+            <div class="tile">
+                <img class="feature_icon" src="/images/icon1.png" alt="значок Mobile Application">
+                <h3 class="tile_header">Mobile Application</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_1">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
 
-        <div class="tile">
-            <img class="feature_icon" src="images/icon3.png" alt="значок User Interface Design">
-            <h3 class="tile_header">User Interface Design</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_3">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
+            <div class="tile">
+                <img class="feature_icon" src="images/icon2.png" alt="значок Web Application">
+                <h3 class="tile_header">Web Application</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_2">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
 
-        <div class="tile">
-            <img class="feature_icon" src="images/icon4.png" alt="значок Strategy and Branding">
-            <h3 class="tile_header">Strategy and Branding</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_4">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
+            <div class="tile">
+                <img class="feature_icon" src="images/icon3.png" alt="значок User Interface Design">
+                <h3 class="tile_header">User Interface Design</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_3">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
 
-        <div class="tile">
-            <img class="feature_icon" src="images/icon5.png" alt="значок Performance Marketing">
-            <h3 class="tile_header">Performance Marketing</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_5">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
+            <div class="tile">
+                <img class="feature_icon" src="images/icon4.png" alt="значок Strategy and Branding">
+                <h3 class="tile_header">Strategy and Branding</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_4">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
 
-        <div class="tile">
-            <img class="feature_icon" src="images/icon6.png" alt="значок Project Management">
-            <h3 class="tile_header">Project Management</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua.
-            </p>
-            <a href="#" class="expand" id="expand_6">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
-        </div>
-        </div>
-         <!-- блок навигации -->
-        <div class="explore">
-            <button type="button">Explore all <img src="images/arrow_r_beige.svg" alt="Стрелка вправо"></button>
-        </div>
-    </main>
+            <div class="tile">
+                <img class="feature_icon" src="images/icon5.png" alt="значок Performance Marketing">
+                <h3 class="tile_header">Performance Marketing</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_5">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
+
+            <div class="tile">
+                <img class="feature_icon" src="images/icon6.png" alt="значок Project Management">
+                <h3 class="tile_header">Project Management</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </p>
+                <a href="#" class="expand" id="expand_6">Expand <img src="images/arrow_r_purple.svg" alt="Развёрнутое описание"></a>
+            </div>
+            </div>
+             <!-- блок навигации -->
+            <div class="explore">
+                <button type="button">Explore all <img src="images/arrow_r_beige.svg" alt="Стрелка вправо"></button>
+            </div>
+        </main>
+    </div>
 
     <footer>
-        <nav>
-            <p class="nav_item"><a class="nav_link" href="math_power.php">Возведение в степень</a></p>
-            <p class="nav_item"><a class="nav_link" href="math_operations.php">Математические операции</a></p>
-        </nav>
        <?php include 'foot.php'; ?>
     </footer>
 </body>
