94f8b36607e77718f9ae82bfcd1020b4d1197f89
dimitryos <dimadb2@yandex.ru>
2022-10-24 18:57:53 +0300

    Дополнен комментарий. Добавлены скобки.

--- shared/docker/site/www/html/src/minutes_rus.php
+++ shared/docker/site/www/html/src/minutes_rus.php
@@ -2,6 +2,7 @@
 
 /**
  * Добавляет к числу слово "минута" в соответствующем падеже.
+ * Падеж определяется на основании формул вида 10k + m = $inputMinutes, где m находится в диапазоне 1..4 и k не равно 1.
  *
  * @param int|string $inputMinutes числовое значение минут
  *
@@ -13,7 +14,7 @@ function minutes_rus(int|string $inputMinutes): string
     $base = "$inputMinutes минут";
 
     return match (true) {
-        $inputMinutes < 0 || $inputMinutes > 59 => 'число_вне_диапазона',
+        ($inputMinutes < 0) || ($inputMinutes > 59) => 'число_вне_диапазона',
         ($inputMinutes - 1) % 10 === 0 && ($inputMinutes - 1) / 10 !== 1 => $base . 'а',
         ($inputMinutes - 2) % 10 === 0 && ($inputMinutes - 2) / 10 !== 1 => $base . 'ы',
         ($inputMinutes - 3) % 10 === 0 && ($inputMinutes - 3) / 10 !== 1 => $base . 'ы',
