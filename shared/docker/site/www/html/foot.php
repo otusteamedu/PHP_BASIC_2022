6fcc843cf330a686030d7ab6a42d35d8184bec0e
dimitryos <dimadb2@yandex.ru>
2022-10-25 15:29:21 +0300

    Добавлена страница с выводом phpinfo()

--- shared/docker/site/www/html/foot.php
+++ shared/docker/site/www/html/foot.php
@@ -1,8 +1,2 @@
- <?php
-    $dtiNow = new DateTimeImmutable('now');
-    $dateNow = $dtiNow->format('d.m.Y');
-    $hoursNow = $dtiNow->format('H');
-    $minutesNow = $dtiNow->format('i');
-?>
-<p><em><small> Сформировано: <?=$dateNow;?>, <?=$hoursNow;?>:<?=$minutesNow;?> </small></em></p>
-<p><em><small> PHP_VERSION: <?=$_SERVER['PHP_VERSION'];?> </small></em></p>
+<p><em><small> Сформировано: <?=date_create()->format('d.m.Y, H:i');?> </small></em></p>
+<p><em><small> PHP_VERSION: <?=$_SERVER['PHP_VERSION'];?>, <a href="info.php">phpinfo</a> </small></em></p>
