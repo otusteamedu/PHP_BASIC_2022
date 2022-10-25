5666e1b8eca856efa0f1837a370b616d5e29d607
dimitryos <dimadb2@yandex.ru>
2022-10-26 01:07:01 +0300

    Переработан вывод фотогалереи

--- shared/docker/site/www/html/index.php
+++ shared/docker/site/www/html/index.php
@@ -1,11 +1,15 @@
 <?php
     $title = 'Фотогалерея';
     require 'head.php';
+    require 'src/treat_form_data.php';
 ?>
 
 <body>
     <header>
-        <h1>Фотогалерея</h1>
+        <h1>
+            <a href="/" class="ml-2 float-left"><img src="images/home.png"></a>
+            Фотогалерея
+        </h1>
     </header>
 
     <main>
@@ -13,22 +17,28 @@
             <div class="row">
                 <!-- Обработать форму и сохранить файлы изображения -->
                 <?php
-                    var_dump($_FILES);
+                    try {
+                        $uploadResult = treat_form_data();
+                    } catch (Exception $e) {
+                        $uploadResult = $e->getMessage();
+                    }
                 ?>
 
                 <!-- Отобразить галерею -->
                 <?php
-                    $imagesUploadFiles = scandir('images/upload/thumbs');
-                    $imageFiles = array_filter($imagesUploadFiles, fn ($file) => boolval(preg_match('/\.(jpg|png|gif)$/', $file)));
+                    $thumbFiles = scandir('images/upload/thumbs');
+                    $thumbFiles = array_filter($thumbFiles, fn ($file) => boolval(preg_match('/^thumb_.*\.png$/', $file)));
 
-                    foreach ($imageFiles as $image) {
-                        $safeImageFileName = htmlspecialchars($image);
+                    foreach ($thumbFiles as $thumb) {
+                        $imageFileName = mb_substr($thumb, 6, -4);
                 ?>
                     <figure class="col-sm-12 col-md-6 col-lg-4">
-                        <a href="<?="images/upload/$safeImageFileName"?>" target="_blank">
-                            <img class="thumb" src="<?="images/upload/$safeImageFileName"?>" alt="<?=$safeImageFileName;?>">
-                        </a>
-                        <figcaption><?=$safeImageFileName;?></figcaption>
+                        <div>
+                            <a href="<?="images/upload/$imageFileName"?>" target="_blank">
+                                <img class="thumb" src="<?="images/upload/thumbs/$thumb"?>" alt="<?=$thumb;?>">
+                            </a>
+                            <figcaption><?=$imageFileName;?></figcaption>
+                        </div>
                     </figure>
                 <?php
                     }
@@ -40,7 +50,11 @@
                 <form method="post" enctype="multipart/form-data">
                     <div class="form-group pt-4">
                         <input type="hidden" name="MAX_FILE_SIZE" value="6000000">
+                        <label for="uploadImageFormControl" class="text-danger"><?=$uploadResult;?></label>
                         <input class="form-control-file" type="file" name="upload" id="uploadImageFormControl">
+                        <label for="uploadImageFormControl" class="font-italic text-info">
+                            <small>jpg-файл не более 6 Мб</small>
+                        </label>
                     </div>
                     <div class="form-group">
                         <input class="form-control" type="submit" name="submit_upload" id="submitUpload">
