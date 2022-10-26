08e1976b8acd100d664fe927a2893c0050aa9be1
eleanorcode <innocence17@gmail.com>
2022-10-27 00:45:46 +0300

    Update paths

--- gallery.php
+++ gallery.php
@@ -3,15 +3,19 @@ require_once 'resize.php';
 
 function gallery()
 {
-    $dir = './img';
-    $files = array_diff(scandir($dir), array('..', '.'));
+
+    $dir1 = dirname(__FILE__) . '/public/img/';
+    $dir2 = dirname(__FILE__) . '/public/mini/';
+
+    $files = array_diff(scandir($dir1), array('..', '.'));
 
     foreach ($files as $value) {
-        $file = "./img/$value";
+        $file = "$dir1" . "$value";
         $image = resizeImage($file, 100, 100);
 
-        imagejpeg($image, "./mini/$value");
+        imagejpeg($image, "$dir2" . "$value");
 
-        echo '<a href="' . $file . '" target = "_blank"><img src="' . './mini/' . $value . '"></a>';
+        echo '<a href="' . './public/img/' . $value . '" target = "_blank"><img src="' . './public/mini/' . $value . '"></a>';
+
     }
 }
