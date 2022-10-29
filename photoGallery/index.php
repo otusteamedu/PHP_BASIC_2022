37641ad7f6480bb8632b65b757eb98b94b9b54d3
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2022-10-29 19:35:31 +0300

    hw_17

--- photoGallery/index.php
+++ photoGallery/index.php
@@ -8,6 +8,7 @@
 
 <body style="text-align: center; background: cadetblue;">
     <h1 style="color: antiquewhite;">Галерея фотографий</h1>
+
     <?php
     require_once('function.php');
     $dir = "img_small";
@@ -16,10 +17,15 @@
     $files = extension($files);
     for ($i = 0; $i < count($files); $i++) {
     ?>
-        <a href="<?= $dir_big . "/" . $files[$i] ?>" target="_blank"><img style="width: 300px; height: 200px" src="<?= $dir . "/" . $files[$i] ?>" alt="Фото галереи" /></a>
+        <a href="<?= $dir_big . "/" . $files[$i] ?>" target="_blank"><img src="<?= $dir . "/" . $files[$i] ?>" alt="Фото галереи" style="width: 500px; height:300px" /></a>
     <?php
     }
     ?>
+
+    <h2 style="color:brown;">Загрузить фотографию</h2>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" name="load" value="Загрузить файл">
 </body>
 
 </html>
