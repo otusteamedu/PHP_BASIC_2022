3ffbe9e649798a983788ad2d7145de7ac2502085
eleanorcode <innocence17@gmail.com>
2022-10-30 17:40:33 +0300

    Change the searchInLibrary function

--- search.php
+++ search.php
@@ -4,30 +4,12 @@ require_once 'db.php';
 
 function searchInLibrary()
 {
-
     if (!empty($_POST['text'])) {
 
         $query = $_POST['text'];
 
-        $data = getBooks();
+        $data = searchBooks($query);
 
-        $counter = 0;
-
-        foreach ($data as $book) {
-
-            if (str_contains($book['title'], $query) || str_contains($book['authors'], $query)) {
-
-                echo '<tr><th scope="row">' . $book['id'] . '</th><td>' . $book['title'] . '</td><td>' . $book['authors'] . '</td><td>' . $book['pages'] . '</td><td>' . $book['year'] . '</td></tr>';
-
-            } else {
-                $counter += 1;
-            }
-        }
-        if ($counter === count($data)) {
-            echo '<tr id="nothing"><th scope="row">' . "Nothing found" . '</th></tr>';
-
-        }
-    } else {
-        header("Location: index.php");
+        return $data;
     }
 }
