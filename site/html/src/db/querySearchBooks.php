b1c242af8936ae560d72f288eace679d24803481
dimitryos <dimadb2@yandex.ru>
2022-10-31 23:21:38 +0300

    Запрос поиска книг

---
+++ site/html/src/db/querySearchBooks.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'db/dbСonnect.php';
+
+/**
+ * Искать в БД список книг, удовлетворяющих поисковому запросу.
+ * 
+ * @return array Список найденных книг.
+ */
+function querySearchBooks(string $searchString): array
+{
+    $conn = dbСonnect();
+    $sql = '
+        SELECT * 
+        FROM `book`
+        WHERE MATCH (`name`, `authors`) AGAINST (?)
+    ';
+    $pstmt = $conn->prepare($sql);
+    $pstmt->execute([$searchString]);
+    $books = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $books;
+}
