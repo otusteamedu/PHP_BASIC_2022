75e2a14867345243d076f079de548e60898523aa
dimitryos <dimadb2@yandex.ru>
2022-10-31 23:28:02 +0300

    Считывание конфигурации проекта и её запись в окружение запроса

---
+++ site/html/src/util/getConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Считать конфигурацию из файла .env и записать её в окружение запроса.
+ * Подразумевается, что файл .env находится в корневой директории проекта (т.е. на уровень выше public).
+ */
+function getConfig(): void
+{
+    $dotEnv = file_get_contents(realpath($_SERVER['DOCUMENT_ROOT'] . '/../.env'));
+    if ($dotEnv === false) {
+        throw new ErrorException("Невозможно прочитать конфигурацию", 1);
+    }
+    $dotEnvAssignments = explode("\n", $dotEnv);
+    array_walk($dotEnvAssignments, fn ($assignment, $index) => $assignment && putenv($assignment));
+}
