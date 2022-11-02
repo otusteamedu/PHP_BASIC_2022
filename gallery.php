82d0bf0140f598427fbc733bda130d64ba42e14c
eleanorcode <innocence17@gmail.com>
2022-11-03 00:44:26 +0300

    Update the gallery function

--- gallery.php
+++ gallery.php
@@ -3,19 +3,19 @@ require_once 'resize.php';
 
 function gallery()
 {
-
     $dir1 = dirname(__FILE__) . '/public/img/';
     $dir2 = dirname(__FILE__) . '/public/mini/';
 
     $files = array_diff(scandir($dir1), array('..', '.'));
+    $miniatures = array_diff(scandir($dir2), array('..', '.'));
 
     foreach ($files as $value) {
-        $file = "$dir1" . "$value";
-        $image = resizeImage($file, 100, 100);
 
-        imagejpeg($image, "$dir2" . "$value");
-
+        if (!in_array($value, $miniatures)) {
+            $file = "$dir1" . "$value";
+            $image = resizeImage($file, 100, 100);
+            imagejpeg($image, "$dir2" . "$value");
+        }
         echo '<a href="' . './img/' . $value . '" target = "_blank"><img src="' . './mini/' . $value . '"></a>';
-
     }
 }
