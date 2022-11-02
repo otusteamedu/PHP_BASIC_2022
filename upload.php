a01d20d7ee4a82aed30aea31da1b3888d0b56be8
eleanorcode <innocence17@gmail.com>
2022-11-02 17:36:40 +0300

    Add login requirement

--- upload.php
+++ upload.php
@@ -1,45 +1,51 @@
 <?php
 
+session_start();
+
 function uploadPhotos()
 {
-    $photos = [];
-    $result = '';
+    if (empty($_SESSION['token'])) {
+        header('Location: public/login.php');
+    } else {
+        $photos = [];
+        $result = '';
 
-    if (file_exists('photos.json')) {
-        $photos = json_decode(file_get_contents('photos.json'), true);
-    }
-
-    if ($_FILES['file']['size'] > 0) {
-        $mimetype = mime_content_type($_FILES['file']['tmp_name']);
-
-        if (in_array($mimetype, array('image/jpeg', 'image/jpg'))) {
-
-            $file = $_FILES['file'];
-
-            $filename = 'public/img/' . uniqid() . '_' . $file['name'];
-            if (move_uploaded_file($file['tmp_name'], $filename)) {
-                $data['file'] = $filename;
-            }
-
-            $photos[] = $data;
-
-            file_put_contents('photos.json', json_encode($photos));
-
-            $result = '<div style="margin-top:50px;font-size:22px;
-  text-align:center;">' . 'Your image have been uploaded!' . '</div>';
-
-        } else {
-
-            $result = '<div style="margin-top:50px;font-size:22px;color:#f00;
-  text-align:center;">' . 'Upload an image in jpg/jpeg format, please' . '</div>';
-
+        if (file_exists('photos.json')) {
+            $photos = json_decode(file_get_contents('photos.json'), true);
         }
 
-    } else {
-        $result = '<div style="margin-top:50px;font-size:22px;color:#f00;
+        if ($_FILES['file']['size'] > 0) {
+            $mimetype = mime_content_type($_FILES['file']['tmp_name']);
+
+            if (in_array($mimetype, array('image/jpeg', 'image/jpg'))) {
+
+                $file = $_FILES['file'];
+
+                $filename = 'public/img/' . uniqid() . '_' . $file['name'];
+                if (move_uploaded_file($file['tmp_name'], $filename)) {
+                    $data['file'] = $filename;
+                }
+
+                $photos[] = $data;
+
+                file_put_contents('photos.json', json_encode($photos));
+
+                $result = '<div style="margin-top:50px;font-size:22px;
+  text-align:center;">' . 'Your image have been uploaded!' . '</div>';
+
+            } else {
+
+                $result = '<div style="margin-top:50px;font-size:22px;color:#f00;
+  text-align:center;">' . 'Upload an image in jpg/jpeg format, please' . '</div>';
+
+            }
+
+        } else {
+            $result = '<div style="margin-top:50px;font-size:22px;color:#f00;
   text-align:center;">' . 'Upload an image, please' . '</div>';
+        }
+        return $result;
     }
-    return $result;
 }
 
 $result = uploadPhotos();
