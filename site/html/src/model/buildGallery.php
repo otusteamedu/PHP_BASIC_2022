e876e613a3f76ec0e2e0e73456ce497ef3dc584c
dimitryos <dimadb2@yandex.ru>
2022-11-08 17:29:24 +0300

    Генерация HTML-разметки элементов галереи

--- site/html/src/model/buildGallery.php
+++ site/html/src/model/buildGallery.php
@@ -8,12 +8,13 @@ function buildGallery(): string
     getConfig();
     $templatesDir = getenv('TEMPLATES_DIR');
     $uploadDir = getenv('UPLOAD_DIR');
+    $thumbsDir = getenv('THUMBS_DIR');
 
     // путь к HTML-шаблону контейнерного элемента для отображения миниатюры
     $pathToImageContainerTemplate = "$templatesDir/image_container.html";
     
     // файлы миниатюр
-    $thumbDirContent = scandir("$uploadDir/thumbs");
+    $thumbDirContent = scandir($thumbsDir);
     $thumbFileNames = array_filter($thumbDirContent, fn ($file) => boolval(preg_match('/^thumb_.*\.png$/', $file)));
     
     // для каждой миниатюры генерируем контейнерный элемент
@@ -21,8 +22,8 @@ function buildGallery(): string
     foreach ($thumbFileNames as $thumbFileName) {
         // исходное название файла изображения
         $imageFileName = mb_substr($thumbFileName, 6, -4);
-        $pathToImageFile = "/images/upload/$imageFileName";
-        $pathToThumbFile = "/images/upload/thumbs/$thumbFileName";
+        $pathToImageFile = '/get_image.php?img=' . urlencode($imageFileName);
+        $pathToThumbFile = "/images/thumbs/$thumbFileName";
 
         $figures .= renderTemplate($pathToImageContainerTemplate, [
             'pathToImageFile' => $pathToImageFile,
