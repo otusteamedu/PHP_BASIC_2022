8767092e6bffe3c0741a36cda15b257f86670b22
dimitryos <dimadb2@yandex.ru>
2022-11-08 17:01:38 +0300

    Скрипт инциализации БД

--- library.db/library.sql
+++ library.db/library.sql
@@ -1,44 +1,96 @@
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB-1:10.9.3+maria~ubu2204
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `book` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-    `authors` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-    `publication_year` SMALLINT(6) NULL DEFAULT NULL,
-    `pages_number` SMALLINT(6) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `authors` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `publication_year` smallint(6) DEFAULT NULL,
+  `pages_number` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  FULLTEXT KEY `book_name_authors_ft` (`name`,`authors`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `book` 
-(`id`, `name`, `authors`, `publication_year`, `pages_number`)
-VALUES 
-(1, _utf8'Основы информатики и вычислительной техники. В двух частях', _utf8'Ершов А. П., Монахов В. М., Бешенков С. А. ', 1985, 96),
-(2, _utf8'Алгоритмы. Руководство по разработке.', _utf8'Скиена Стивен', 2011, 720),
-(3, _utf8'Математическая логика и теория алгоритмов', _utf8'Игошин В.И. ', 2008, 448),
-(4, _utf8'UNIX. Программное окружение', _utf8'Брайан Керниган, Роб Пайк', 2017, 410),
-(5, _utf8'Язык программирования C', _utf8'Керниган Брайан, Ритчи Деннис', 2019, 288),
-(6, _utf8'PHP 7', _utf8'Д. В. Котеров, И. В. Симдянов', 2016, 1088),
-(7, _utf8'PHP 5', _utf8'Дмитрий Котеров, Алексей Костарев', 2008, 1094),
-(8, _utf8'Самоучитель PHP 7', _utf8'Симдянов Игорь Вячеславович, Кузнецов Максим', 2018, 448),
-(9, _utf8'SQL. Сборник рецептов. 2-е изд.', _utf8'Энтони Молинаро, Роберт де Грааф', 2021, 592),
-(10, _utf8'PostgreSQL. Основы языка SQL', _utf8'Е. П. Моргунов', 2019, 336)
-;
+--
+-- Dumping data for table `book`
+--
 
-CREATE FULLTEXT INDEX IF NOT EXISTS `book_name_authors_ft`
-ON `book` (`name`, `authors`)
-;
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES
+(1,'Основы информатики и вычислительной техники. В двух частях','Ершов А. П., Монахов В. М., Бешенков С. А. ',1985,96),
+(2,'Алгоритмы. Руководство по разработке.','Скиена Стивен',2011,720),
+(3,'Математическая логика и теория алгоритмов','Игошин В.И. ',2008,448),
+(4,'UNIX. Программное окружение','Брайан Керниган, Роб Пайк',2017,410),
+(5,'Язык программирования C','Керниган Брайан, Ритчи Деннис',2019,288),
+(6,'PHP 7','Д. В. Котеров, И. В. Симдянов',2016,1088),
+(7,'PHP 5','Дмитрий Котеров, Алексей Костарев',2008,1094),
+(8,'Самоучитель PHP 7','Симдянов Игорь Вячеславович, Кузнецов Максим',2018,448),
+(9,'SQL. Сборник рецептов. 2-е изд.','Энтони Молинаро, Роберт де Грааф',2021,592),
+(10,'PostgreSQL. Основы языка SQL','Е. П. Моргунов',2019,336);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`login` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`password` VARCHAR(150) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`roles` TEXT NOT NULL COLLATE 'utf8mb4_bin',
-	PRIMARY KEY (`id`) USING BTREE,
-	CONSTRAINT `roles` CHECK (json_valid(`roles`))
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `display_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `remember_token` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  CONSTRAINT `roles` CHECK (json_valid(`roles`))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `user` (`id`, `login`, `password`, `roles`) VALUES (1, 'vagrant', '$argon2id$v=19$m=65536,t=4,p=1$dXJHMHNMZmh0RU1TYjFXeA$9ULKgR8gz1nJ3QRZ2gDvdwNYW6/47BNxKlxJexRXShc', '[]');
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES
+(1,'vagrant','$argon2id$v=19$m=65536,t=4,p=1$dXJHMHNMZmh0RU1TYjFXeA$9ULKgR8gz1nJ3QRZ2gDvdwNYW6/47BNxKlxJexRXShc','Вагрантик','[\"ROLE_USER\", \"ROLE_ADMIN\"]',NULL),
+(2,'tarapunka','$argon2id$v=19$m=65536,t=4,p=1$QzlPWmM4MUlQM0RTalEudQ$cSP6dW1y+YRKYzekn5E56KpdV/zMZA2zcRDqjGSN2/g','Тарапунька','[\"ROLE_USER\"]',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-08 16:27:05
