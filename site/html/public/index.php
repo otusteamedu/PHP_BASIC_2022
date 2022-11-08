33960e5b55408adc2a7693afa6e4ad29a4b8f4ca
dimitryos <dimadb2@yandex.ru>
2022-11-08 17:11:34 +0300

    Добавлена логика аутентификации по remember, учёта привелегий и рендеринга

--- site/html/public/index.php
+++ site/html/public/index.php
@@ -1,46 +1,57 @@
 <?php
+    require 'vendor/autoload.php';
+    
+    // аутентифицировать по remember-токену, если он есть
+    rememberUser();
+
     $title = 'Фотогалерея';
     require 'head.php';
-
-    require 'vendor/autoload.php';
-
-    // Обработать форму и сохранить файлы изображения
-    $uploadResult = treatFormData();
-
-    // Сформировать галерею
-    $images = buildGallery();
 ?>
 
 <body>
     <header>
-        <h1>
-            <a href="/" class="ml-2 float-left">
-                <img src="images/home.png">
-            </a>
-            Фотогалерея
-        </h1>
+        <nav class="navbar navbar-expand-lg justify-content-between">
+            <a href="/" class="navbar-brand"> <img src="/images/home.png"> </a>
+            <h1 class="navbar-text"> Фотогалерея </h1>
+
+            <!-- Блок пользователя -->
+            <?php if (isGranted()) {
+                // Показываем кнопку выхода, если вход был успешный
+                echo renderTemplate('templates/logout_form.html', [
+                    'display_name' => $_SESSION['display_name'],
+                    'csrf_token' => getCsrfToken('logout_csrf_token')
+                ]);
+            } else {
+                // Показываем форму входа, если логин ещё не был произведён
+                echo renderTemplate('templates/login_form.html', [
+                    'login' => $_SESSION['login'] ?? '',
+                    'csrf_token' => getCsrfToken('login_csrf_token'),
+                    'form_error' => $_SESSION['login_form_error'] ?? ''
+                ]);
+            } ?>
+        </nav>
     </header>
 
     <main>
         <div class="container-fluid">
-            <div class="row"> <?=$images;?> </div>
+            <!-- Отображаем галерею -->
+            <?php if (isGranted()) {
+                // Сформировать галерею
+                $images = buildGallery();
+                echo sprintf('<div class="row">%s</div>', $images);
+            } else {
+                echo renderTemplate('templates/alert_warning.html', [
+                    'message' => 'Войдите на сайт для просмотра фотогалереи.'
+                ]);
+            } ?>
 
-            <!-- Форма загрузки картинки -->
-            <div class="d-flex justify-content-center">
-                <form method="post" enctype="multipart/form-data">
-                    <div class="form-group pt-4">
-                        <input type="hidden" name="MAX_FILE_SIZE" value="6000000">
-                        <label for="uploadImageFormControl" class="text-danger"><?=$uploadResult;?></label>
-                        <input class="form-control-file" type="file" name="upload" id="uploadImageFormControl">
-                        <label for="uploadImageFormControl" class="font-italic text-info">
-                            <small>jpg-файл не более 6 Мб</small>
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" type="submit" name="submit_upload" id="submitUpload">
-                    </div>
-                </form>
-            </div>
+            <!-- Форма загрузки картинки для админов -->
+            <?php if (isGranted('ROLE_ADMIN')) {
+                echo renderTemplate('templates/upload_form.html', [
+                    'csrf_token' => getCsrfToken('upload_csrf_token'),
+                    'form_error' => $_SESSION['upload_form_error'] ?? ''
+                ]);
+            } ?>
         </div>
     </main>
 
