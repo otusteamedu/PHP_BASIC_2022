45a318e91f2bd9dd982466e8095ba11b535619ea
dimitryos <dimadb2@yandex.ru>
2022-11-08 17:05:24 +0300

    Добавлена ссылка на страницу отображения phpinfo() для ROLE_ADMIN

--- site/html/public/foot.php
+++ site/html/public/foot.php
@@ -1,4 +1,7 @@
 
 <nav class="d-flex justify-content-between">
     <p><em><small> Сформировано: <?=date_create()->format('d.m.Y, H:i');?> </small></em></p>
+    <?php if (isGranted("ROLE_ADMIN")) { ?>
+        <p><a href="/info.php">info</a></p>
+    <?php } ?>
 </nav>
