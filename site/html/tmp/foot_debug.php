fccbefcfb7127c15a9ad7940dce8b72a6561c4b9
dimitryos <dimadb2@yandex.ru>
2022-11-08 22:01:53 +0300

    Удалена временная директория tmp

--- site/html/tmp/foot_debug.php
+++
@@ -1,6 +0,0 @@
-
-<nav class="d-flex justify-content-between">
-    <p><em><small> Сформировано: <?=date_create()->format('d.m.Y, H:i');?> </small></em></p>
-    <p><em><small> <a href="info.php">phpinfo</a> </small></em></p>
-</nav>
-<p><?=$debug;?></p>
