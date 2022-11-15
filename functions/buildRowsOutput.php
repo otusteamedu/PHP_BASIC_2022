7ce6375ea202b0dce2634d404a4fadc4324d6fe9
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2022-11-15 13:58:11 +0300

    hw_18

--- functions/buildRowsOutput.php
+++ functions/buildRowsOutput.php
@@ -2,10 +2,10 @@
 include 'renderTemplate.php';
 function buildRowsOutput($books)
 {
-    $pathTemp = "../templates/templateOutput.html";
+    $pathTempOutput = "../templates/templateOutput.html";
     $booksTableRowsHtml = '';
     foreach ($books as $book) {
-        $booksTableRowsHtml .= renderTemplate($pathTemp, [
+        $booksTableRowsHtml .= renderTemplate($pathTempOutput, [
             'Id_1'     => $book['id_book'],
             'name_2'   => htmlspecialchars($book['name']),
             'pages_3'  => htmlspecialchars($book['pages']),
