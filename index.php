8b7dbe87c05beb27cbf4ae239b155cf3781544e4
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2022-11-22 23:47:13 +0300

    hw_19

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+require 'conn/authenticate.php';
 session_start();
 if (empty($_SESSION['token'])) {
-    header('Location: /auth.php');
+    header('Location: template/authForm.php');
 } else {
     header('Location: template/templateAdmin.php');
+}
+
+if (!empty($_POST['user']) && !empty($_POST['password'])) {
+    if (authenticate($_POST['user'], $_POST['password'])) {
+        $_SESSION['username'] = $_POST['user'];
+        $_SESSION['token'] = uniqid();
+    } else {
+        header('Location: template/templateGuest.php');
+    }
 }
