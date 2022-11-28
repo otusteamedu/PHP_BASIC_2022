c48acf293f65d07c7520504fc06c59c3ca91ba43
dimitryos <dimadb2@yandex.ru>
2022-11-28 22:20:09 +0300

    Класс "Посылка 1 класса"

---
+++ src/p1234/FirstClassParcel.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Посылка 1 класса
+ */
+final class FirstClassParcel extends Parcel
+{
+    /**
+     * @var int объявленная стоимость
+     */
+    private int $declaredValue = 0;
+
+    /**
+     * @var int наложенный платёж
+     */
+    private int $cashOnDelivery = 0;
+
+    /**
+     * @var array опись вложения
+     */
+    private array $contentsList = [];
+
+    public function __construct(
+        int $weight,
+        int $height,
+        int $width,
+        int $length,
+        string $toAddress,
+        string $fromAddress,
+        string $trackNumber
+    ) {
+        // особые ограничения на габариты и вес посылки для данной категории
+        if ($weight > 5000) {
+            throw new InvalidArgumentException("Превышен максимальный вес", 1);
+        }
+        if ($height + $width + $length > 70 || $height > 36 || $width > 36 || $length > 36) {
+            throw new InvalidArgumentException("Превышены максимальные габариты", 1);
+        }
+
+        parent::__construct($weight, $height, $width, $length, $toAddress, $fromAddress, $trackNumber);
+    }
+
+    public function getDeclaredValue(): int
+    {
+        return $this->declaredValue;
+    }
+
+    public function setDeclaredValue(int $declaredValue): self
+    {
+        $this->declaredValue = $declaredValue;
+
+        return $this;
+    }
+
+    public function getCashOnDelivery(): int
+    {
+        return $this->cashOnDelivery;
+    }
+
+    public function setCashOnDelivery(int $cashOnDelivery): self
+    {
+        $this->cashOnDelivery = $cashOnDelivery;
+
+        return $this;
+    }
+
+    public function getContentsList(): array
+    {
+        return $this->contentsList;
+    }
+
+    public function setContentsList(array $contentsList): self
+    {
+        $this->contentsList = $contentsList;
+
+        return $this;
+    }
+
+    /**
+     * Расчёт итоговой цены посылки в копейках
+     * 
+     * @return int
+     */
+    public function getPrice(): float
+    {
+        // базовая цена отправления
+        $price = parent::getPrice();
+
+        // некий алгоритм наценки за услуги
+        $price += $this->declaredValue / 3.39;
+        // была заказана опись вложения
+        $price += !empty($this->contentsList) ? 12000.0 : 0.0;
+
+        return ceil($price);
+    }
+}
