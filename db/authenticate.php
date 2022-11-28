e55715a356fdd3638dc69d087124fabea22f928c
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2022-11-28 11:24:26 +0300

    hw_19

--- db/authenticate.php
+++ db/authenticate.php
@@ -9,3 +9,20 @@ function authenticate($username)
     $hash_pass = $output[0]['password'];
     return $hash_pass;
 }
+
+session_start();
+if (empty($_SESSION['token'])) {
+    header('Location: template/authForm.php');
+} else {
+    header('Location: template/templateAdmin.php');
+}
+
+if (!empty($_POST['user']) && !empty($_POST['password'])) {
+    if (password_verify($_POST['password'], authenticate($_POST['user']))) {
+        header('Location: template/templateAdmin.php');
+        $_SESSION['username'] = $_POST['user'];
+        $_SESSION['token'] = uniqid();
+    } else {
+        header('Location: template/templateGuest.php');
+    }
+}
