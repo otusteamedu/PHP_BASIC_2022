3d4ae1f58ccc29ea7ab93312ef825acb1d7f1b67
eleanorcode <innocence17@gmail.com>
2023-01-08 23:24:14 +0300

    Add 404 page

---
+++ www/src/Views/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>404 - Not Found</title>
+  <style>
+  h1,
+  h2 {
+    text-align: center;
+  }
+  </style>
+</head>
+
+<body>
+  <h1>404</h1>
+  <h2>Page not found</h2>
+</body>
+
+</html>
