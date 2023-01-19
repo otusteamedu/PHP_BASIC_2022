82e4da7e4cd6d2e5fd851a89707bc3fdcf5a7458
eleanorcode <innocence17@gmail.com>
2023-01-19 20:30:53 +0300

    Update authenticate method

--- www/Models/Auth.php
+++ www/Models/Auth.php
@@ -6,13 +6,16 @@ use HW\HW18\Core\Database;
 class Auth {
 
   public static function authenticate($username, $password): bool {
-
     $pdo = Database::connect();
 
     $data = $pdo->prepare('SELECT 1 FROM users where username = ? and password = ?');
-    $data->execute([$username, md5($password)]);
-    $result = $data->fetchAll();
+    $data->execute([$username, hash('sha256', $password)]);
+    $result = $data->fetch();
 
-    return count($result) > 0;
+    if ($result) {
+      return (password_verify($result["password"], $result["password_hash"]));
+    } else {
+      return $result;
+    }
   }
 }
