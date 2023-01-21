8436c0217c572400decea3585f7694974f1eaec1
eleanorcode <innocence17@gmail.com>
2023-01-21 12:31:43 +0300

    Update sql-query

--- www/Models/Auth.php
+++ www/Models/Auth.php
@@ -6,9 +6,10 @@ use HW\HW18\Core\Database;
 class Auth {
 
   public static function authenticate($username, $password): bool {
+
     $pdo = Database::connect();
 
-    $data = $pdo->prepare('SELECT 1 FROM users where username = ? and password = ?');
+    $data = $pdo->prepare('SELECT * FROM users where username = ? and password = ?');
     $data->execute([$username, hash('sha256', $password)]);
     $result = $data->fetch();
 
