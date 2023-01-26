583a3f2489bdb9344218de27eda0d595317a170e
eleanorcode <innocence17@gmail.com>
2023-01-27 02:04:30 +0300

    Add session methods

--- App/Models/Auth.php
+++ App/Models/Auth.php
@@ -30,4 +30,21 @@ class Auth {
       return $result;
     }
   }
+  public static function set($param, $value) {
+    session_start();
+    if (!array_key_exists($param, $_SESSION)) {
+      $_SESSION[$param] = $value;
+    }
+  }
+  public static function get($param, $default = '') {
+    session_start();
+    if (array_key_exists($param, $_SESSION)) {
+      return $_SESSION[$param];
+    }
+    return $default;
+  }
+  public static function logout() {
+    session_start();
+    session_destroy();
+  }
 }
