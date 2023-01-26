99103f90ca82a92a8c449bb8b8246b973b6866db
eleanorcode <innocence17@gmail.com>
2023-01-27 02:28:24 +0300

    Fix errors

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -1,20 +1,12 @@
 <?php
 
 namespace App\Controllers;
-use App\Core\Redirect;
-use App\Core\Session;
 use App\Core\View;
 
 class IndexController {
   public function index() {
-    $user = Session::get('username');
-    if (!empty($user)) {
-      View::render('personal', [
-        'title' => 'Personal page of ' . $user,
-        'name' => $user,
-      ]);
-    } else {
-      Redirect::redirect('/');
-    }
+    View::render('auth', [
+      'title' => 'Home page',
+    ]);
   }
 }
