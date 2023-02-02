73be5d49cdf21f883989178c0d52e97a1e92e72d
Nikita <nick99@bk.ru>
2023-02-03 01:54:36 +0300

    1

--- hw4.php
+++ hw4.php
@@ -10,6 +10,21 @@
 </head>
 <body>
 	<section class="main">
+		<?php
+		if (isset($_FILES["userfile"]["type"])){
+		    if(strstr($_FILES["userfile"]["type"], 'image')){
+		    	include('lib/resize.php');
+				$tmp_name = $_FILES["userfile"]["tmp_name"];
+				$salt = uniqid();
+		    	$name = "img/".$salt."_".$_FILES["userfile"]["name"];
+		    	move_uploaded_file($tmp_name, $name);
+		    	imagepng(resize_image($name, 100, 100), "img/thumbs/".$salt."_".$_FILES["userfile"]["name"]);
+		    } else {
+		    	echo "Не допустимый формат файла.";
+		    }
+		}
+
+		?>
 
 		<div class="flex_container" >
 			<?php
@@ -17,11 +32,11 @@
 
 				$count = count($listFiles);
 				for($i=0; $i<$count; $i++){
-					if (is_file("img/".$listFiles[$i])){
+					if (is_file("img/".$listFiles[$i]) && is_file("img/thumbs/".$listFiles[$i])){
 						?>
 						<div class="services" >
 							<a href="/img/<?=$listFiles[$i]?>" target="_blank" >
-								<img src="/img/<?=$listFiles[$i]?>">
+								<img src="/img/thumbs/<?=$listFiles[$i]?>">
 							</a>
 						</div>
 						<?
@@ -32,7 +47,7 @@
 		</div>
 
 		<div class="button">
-			<form enctype="multipart/form-data" action="upload.php" method="POST">
+			<form enctype="multipart/form-data" action="" method="POST">
 				Загрузить изображение: <input name="userfile" type="file" />
 				<input class="button" type="submit" value="Загрузить" />
 			</form>
