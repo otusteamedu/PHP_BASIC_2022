806f70c0d182dae701b69c87ab1929e437cbf3cc
Nikita <nick99@bk.ru>
2023-02-04 22:48:11 +0300

    2

--- hw4.php
+++ hw4.php
@@ -1,3 +1,7 @@
+<?php
+include ('lib/functions.php');
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -11,36 +15,18 @@
 <body>
 	<section class="main">
 		<?php
-		if (isset($_FILES["userfile"]["type"])){
-		    if(strstr($_FILES["userfile"]["type"], 'image')){
-		    	include('lib/resize.php');
-				$tmp_name = $_FILES["userfile"]["tmp_name"];
-				$salt = uniqid();
-		    	$name = "img/".$salt."_".$_FILES["userfile"]["name"];
-		    	move_uploaded_file($tmp_name, $name);
-		    	imagepng(resize_image($name, 100, 100), "img/thumbs/".$salt."_".$_FILES["userfile"]["name"]);
-		    } else {
-		    	echo "Не допустимый формат файла.";
-		    }
-		}
+		if (isset($_FILES) && $_FILES){
+			$resultUpload = uploadFile();
+			if ($resultUpload['error'])
+	    		echo $resultUpload['text']."<br>";
+	    }
 
 		?>
-
 		<div class="flex_container" >
 			<?php
-				$listFiles = scandir('img');
-
-				$count = count($listFiles);
-				for($i=0; $i<$count; $i++){
-					if (is_file("img/".$listFiles[$i]) && is_file("img/thumbs/".$listFiles[$i])){
-						?>
-						<div class="services" >
-							<a href="/img/<?=$listFiles[$i]?>" target="_blank" >
-								<img src="/img/thumbs/<?=$listFiles[$i]?>">
-							</a>
-						</div>
-						<?
-					}
+				$listFoto = getImagesList('img');
+				foreach ($listFoto as $fileName) {
+				    echo getImageTpl($fileName, 'img');
 				}
 			?>
 			
