eea4a63d8fc957baec1b4de40538bd9ba41a087c
Nikita <nick99@bk.ru>
2023-02-13 23:14:19 +0300

    2

--- hw9.php
+++ hw9.php
@@ -34,10 +34,7 @@ class Drink extends Product{
 	public float $vol;
 	public function __construct (string $name, string $group, int $price, int $count, float $vol)
 	{
-		$this->name = $name;
-		$this->group = $group;
-		$this->price = $price;
-		$this->count = $count;
+		parent::__construct($name, $group, $price, $count);
 		$this->vol = $vol;
 	}
 }
@@ -46,10 +43,7 @@ class Lamp extends Product{
 	public float $base;
 	public function __construct (string $name, string $group, int $price, int $count, int $base)
 	{
-		$this->name = $name;
-		$this->group = $group;
-		$this->price = $price;
-		$this->count = $count;
+		parent::__construct($name, $group, $price, $count);
 		$this->base = $base;
 	}
 
@@ -62,22 +56,5 @@ class Lamp extends Product{
 $lamp = new Lamp('Lamp', 'Electric', 10, 20, 6);
 
 $beer = new Drink('Beer', 'Alcohol', 30, 60, 0.5);
-
-//Что он выведет на каждом шаге? Почему?
-//Немного изменим п.5
-class A {
-	public function foo() {
-		static $x = 0;
-		echo ++$x;
-	}
-}
-class B extends A {
-}
-$a1 = new A();
-$b1 = new B();
-echo $a1->foo();
-echo $b1->foo();
-echo $a1->foo();
-echo $b1->foo();
 
 ?>
