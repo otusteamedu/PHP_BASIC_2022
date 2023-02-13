7ee2ded1a2d6659ebab16eb83273707fe5272d6f
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2023-02-13 23:36:40 +0300

    hw_27

--- framework/Core/App.php
+++ framework/Core/App.php
@@ -1,36 +1,35 @@
 <?php
 
 namespace Otus\Mvc\Core;
+
 use Dotenv\Dotenv;
+use Otus\Mvc\Core\Route;
 
 class App
 {
-    protected static $routes = [
-        'lessons/linux-specialization' => ['Lessons','linux'],
-        'linux-spec' => ['Lessons','linux']
-    ];
 
-    public static function run()
+
+    public function run()
     {
         Database::bootEloquent();
 
         $controller_name = "Otus\\Mvc\\Controllers\\IndexController";
         $action_name = "index";
-
+        $routes = new Route();
+        $routes = $routes->routes;
         $path = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");
-        if(array_key_exists($path,self::$routes))
-        {
-            $controller = self::$routes[$path][0];
+
+        if (array_key_exists($path, $routes)) {
+            $controller = $routes[$path][0];
             $controller_name = "Otus\\Mvc\\Controllers\\{$controller}Controller";
-            $action_name = self::$routes[$path][1];
+            $action_name = $routes[$path][1];
         } else {
-            if($path !== "")
-            {
+            if ($path !== "") {
                 @list($controller, $action) = explode("/", $path, 2);
-                if (isset($controller)){
+                if (isset($controller)) {
                     $controller_name = "Otus\\Mvc\\Controllers\\{$controller}Controller";
                 }
-                if (isset($action)){
+                if (isset($action)) {
                     $action_name = $action;
                 }
             }
@@ -38,20 +37,17 @@ class App
 
 
         // Check controller exists.
-        if(!class_exists($controller_name,true)) {
+        if (!class_exists($controller_name, true)) {
             //redirect to 404
             View::render('404');
         }
 
-        if(!method_exists($controller_name, $action_name)) {
+        if (!method_exists($controller_name, $action_name)) {
             //redirect to 404
             View::render('404');
         }
 
         $controller = new $controller_name();
         $controller->$action_name();
-
-
-
     }
 }
---
+++ framework/config/configer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Otus\Mvc\Core;
+
+class Route
+{
+    public $routes = array(
+        'lessons/linux-specialization' => ['Lessons', 'linux'],
+        'linux-spec' => ['Lessons', 'linux']
+    );
+}
