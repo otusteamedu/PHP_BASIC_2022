6a1726e625394af0daf4baff5541308086e2eaa4
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2023-02-15 01:08:59 +0300

    hw_25

--- framework/Core/App.php
+++ framework/Core/App.php
@@ -5,8 +5,8 @@ namespace Otus\Mvc\Core;
 class App
 {
     protected static $routes = [
-        'lessons/linux-specialization' => ['Lessons','linux'],
-        'linux-spec' => ['Lessons','linux']
+        'lessons/linux-specialization' => ['Lessons', 'linux'],
+        'linux-spec' => ['Lessons', 'linux']
     ];
 
     public static function run()
@@ -17,19 +17,17 @@ class App
         $action_name = "index";
 
         $path = trim(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), "/");
-        if(array_key_exists($path,self::$routes))
-        {
+        if (array_key_exists($path, self::$routes)) {
             $controller = self::$routes[$path][0];
             $controller_name = "Otus\\Mvc\\Controllers\\{$controller}Controller";
             $action_name = self::$routes[$path][1];
         } else {
-            if($path !== "")
-            {
+            if ($path !== "") {
                 @list($controller, $action) = explode("/", $path, 2);
-                if (isset($controller)){
+                if (isset($controller)) {
                     $controller_name = "Otus\\Mvc\\Controllers\\{$controller}Controller";
                 }
-                if (isset($action)){
+                if (isset($action)) {
                     $action_name = $action;
                 }
             }
@@ -37,17 +35,23 @@ class App
 
 
         // Check controller exists.
-        if(!class_exists($controller_name,true)) {
+        if (!class_exists($controller_name, true)) {
             //redirect to 404
             View::render('404');
         }
 
-        if(!method_exists($controller_name, $action_name)) {
+        if (!method_exists($controller_name, $action_name)) {
             //redirect to 404
             View::render('404');
         }
 
         $controller = new $controller_name();
         $controller->$action_name();
+    }
+
+    public static function devMode()
+    {
+        $isDevMode = true;
+        return $isDevMode;
     }
 }
--- framework/Core/Database.php
+++ framework/Core/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Otus\Mvc\Core;
+
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 use Doctrine\ORM\Tools\Setup;
@@ -10,11 +11,10 @@ class Database
 {
     public static function config()
     {
-        if(PHP_SAPI == "cli")
-        {
-            return require implode(DIRECTORY_SEPARATOR,[__DIR__,'..','config','database.php']);
+        if (PHP_SAPI == "cli") {
+            return require implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'config', 'database.php']);
         }
-        return require implode(DIRECTORY_SEPARATOR,[$_SERVER['DOCUMENT_ROOT'],'config','database.php']);
+        return require implode(DIRECTORY_SEPARATOR, [$_SERVER['DOCUMENT_ROOT'], 'config', 'database.php']);
     }
 
     public static function bootEloquent()
@@ -35,11 +35,11 @@ class Database
 
     public static function bootDoctrine()
     {
-        $isDevMode = true;
+        $isDevMode = App::devMode();
         $proxyDir = null;
         $cache = null;
         $useSimpleAnnotationReader = false;
-        $path = PHP_SAPI == "cli" ? implode(DIRECTORY_SEPARATOR, [__DIR__,'..','Models','Doctrine']) : implode(DIRECTORY_SEPARATOR,[$_SERVER['DOCUMENT_ROOT'],'Models','Doctrine']);
+        $path = PHP_SAPI == "cli" ? implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Models', 'Doctrine']) : implode(DIRECTORY_SEPARATOR, [$_SERVER['DOCUMENT_ROOT'], 'Models', 'Doctrine']);
         $config = Setup::createAnnotationMetadataConfiguration(array($path), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
 
         $conn = self::config();
